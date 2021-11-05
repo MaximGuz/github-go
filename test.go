d1281653919a7b089a585c0f95ec65b164a31a3d
MaximGuz <guz.maks@list.ru>
2021-11-05 17:32:48 +0300

    Пакет main с программой трехзначного числа

---
+++ test.go
@@ -0,0 +1,12 @@
+//Программа получает на вход трехзначное число и выводит сумму его чисел
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	a := n/100 + n/10%10 + n%10
+	fmt.Println(a)
+
+}
